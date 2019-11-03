d9d7b5035e873b764abf8819ffe2e73febfd9b6d
adhamelk <adham.el-khouly@mail.mcgill.ca>
2019-11-03 16:43:49 -0500

    added functions to generate, read, and write a random file

--- src/file_manipulation.h
+++ src/file_manipulation.h
@@ -4,5 +4,10 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include <time.h>
+
 char* read_file(char *fileName);
+int write_file(char *file_name, char* output_str);
+int generate_file(int size);
+
 #endif
