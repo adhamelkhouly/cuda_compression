c06d66ba032999a58a0980cf1c649328265a6a5c
Moetassem Abdelazim <moetassem.abdelazim@mail.mcgill.ca>
2019-11-16 17:32:35 -0500

    Fixed encoding in parallel, briefly tested and seems to work - not yet fully done

--- src/parallel/lzAsc/lz_ascii.cu
+++ src/parallel/lzAsc/lz_ascii.cu
@@ -1,34 +1,32 @@
 #include "lz_ascii.h"
 
-using namespace std;
-
 /****************Cuda Functions on GPU*************************/
-__global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_block, uint8_t* dev_in, uint8_t* out[], lzw_enc_t* dict, size_t size, int max_bits)
+__global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_block, uint8_t* dev_in, uint8_t* out_final, lzw_enc_t* dict, size_t size, int max_bits)
 {
-	int size_per_thread = size / num_of_threads;
-	int segment_num = size_per_thread * (threadIdx.x + (blockIdx.x * threads_per_block));
-	
-	uint8_t* segment_input_ptr = &dev_in[segment_num];
+	int size_per_thread_const = size / num_of_threads;
+	int size_per_thread_change = size_per_thread_const;
+	int segment_num = (threadIdx.x + (blockIdx.x * threads_per_block));
+
+	uint8_t* segment_input_ptr = &dev_in[segment_num* size_per_thread_const];
 
 	int bits = 9, next_shift = 512;
 	uint16_t code, c, nc, next_code = M_NEW;
-	size_t out_segment_size = sizeof(size_t) * 2 + 4 * sizeof(uint16_t);
+	//size_t out_segment_size = sizeof(size_t) * 2 + 4 * sizeof(uint16_t);
 
 	if (max_bits > 15) max_bits = 15;
 	if (max_bits < 9) max_bits = 12;
 
-	//size_t* y = new uint16_t[]
+	//size_t* y = (size_t*)malloc(out_segment_size);
+	//y[0] = sizeof(uint16_t);
+	//y[1] = 4;
+	//printf("%i\n", segment_num);
+	//out_final[segment_num] = (uint8_t*)(y[2]);
 
-	size_t* y = (size_t*)malloc(sizeof(size_t) * 2 + 4 * sizeof(uint16_t));
-	y[0] = sizeof(uint16_t);
-	y[1] = 4;
-	out[segment_num] = (uint8_t*)(y + 2);
-
-	//out[segment_num] = (uint8_t*)gpu_mem_alloc(sizeof(uint16_t), 4);
+	////out[segment_num] = (uint8_t*)gpu_mem_alloc(sizeof(uint16_t), 4);
 	int out_len = 0, o_bits = 0;
 	uint32_t tmp = 0;
 
-	for (code = *(segment_input_ptr++); --size_per_thread; ) {
+	for (code = *(segment_input_ptr++); --size_per_thread_change; ) {
 		c = *(segment_input_ptr++);
 		if ((nc = dict[code].next[c])) //if nc is not equal to 0 after assignment then enter if statment
 			code = nc;
@@ -36,14 +34,16 @@ __global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_
 			///
 			tmp = (tmp << bits) | code; //shifting tmp 9 bits to the left (multiplying 2^9) then or with code (an ascii variable or new added one e.x code of 'ab')
 			o_bits += bits;
-			if (_len(out[segment_num]) <= out_len) {
-				size_t new_n = _len(out[segment_num]) * 2;
-				size_t* z = (size_t*)(out[segment_num] - 2); //go back two size_t's (64 bits in our definition) to get the previously stored item_size and number of items
-				cudaError_t cudaStatus = cudaMalloc((void**)& z, sizeof(size_t) * 2 + *z * new_n); //
-				if (new_n > z[1]) //if actually more memory is asked for then initialize the extra with zeros till we fill it out in the future
-					memset((char*)(z + 2) + z[0] * z[1], 0, z[0] * (new_n - z[1]));
-				z[1] = new_n;
-				out[segment_num] = (uint8_t*)(z + 2);
+			if (size_per_thread_const <= out_len) {
+				printf("\nEncoding using more momery in this block ... Exiting ... Could be done better to accomodate for extra space per block \n");
+				return;
+				//size_t new_n = _len(out[segment_num]) * 2;
+				//size_t* z = (size_t*)(out[segment_num] - 2); //go back two size_t's (64 bits in our definition) to get the previously stored item_size and number of items
+				//cudaError_t cudaStatus = cudaMalloc((void**)& z, sizeof(size_t) * 2 + *z * new_n); //
+				//if (new_n > z[1]) //if actually more memory is asked for then initialize the extra with zeros till we fill it out in the future
+				//	memset((char*)(z + 2) + z[0] * z[1], 0, z[0] * (new_n - z[1]));
+				//z[1] = new_n;
+				//out[segment_num] = (uint8_t*)(z + 2);
 
 				//out[segment_num] = (uint8_t*)gpu_mem_extend(out[segment_num], _len(out[segment_num]) * 2); //extend by doubling size
 			}
@@ -52,16 +52,18 @@ __global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_
 
 				//shifting o_bits to the right, shifting to the right means dividing by 2^(o_bits)
 				//eleminating the leftover bits on the right to write one byte to the ouput
-				out[segment_num][out_len++] = tmp >> o_bits;
-				//printf("%i" , out[segment_num][out_len-1]);
-
+				out_final[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
+				out_len++;
+				
 				//shift 1 to the left by o_bits, basically multiplying 1 by 2^(o_bits) ... then mask this value-1 on tmp
 				//saving the leftover bits on the right from the previous line for the next iteration
 				//e.x 1110 1110 11, tmp will be the 11 at the right
 				tmp &= (1 << o_bits) - 1;
 			}
 			///
+			_Acquires_exclusive_lock_();
 			nc = dict[code].next[c] = next_code++;
+			_Releases_exclusive_lock_();
 			code = c;
 		}
 	}
@@ -142,7 +144,7 @@ int main(int argc, char* argv[])
 cudaError_t lz_ascii_with_cuda(uint8_t* in, int numOfThreads)
 {
 	uint8_t* dev_in = 0;
-	uint8_t* dev_final_out[] = { 0 };
+	uint8_t* dev_final_out = 0;
 	size_t* x = 0;
 
 	// Choose which GPU to run on, change this on a multi-GPU system.
@@ -152,7 +154,7 @@ cudaError_t lz_ascii_with_cuda(uint8_t* in, int numOfThreads)
 		goto Error;
 	}
 
-	cudaStatus = cudaMallocManaged((void**)& dev_in, _len(in) * sizeof(unsigned char));
+	cudaStatus = cudaMallocManaged((void**)& dev_in, _len(in) * sizeof(uint8_t));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
 		goto Error;
@@ -160,11 +162,13 @@ cudaError_t lz_ascii_with_cuda(uint8_t* in, int numOfThreads)
 
 	cudaMemcpy(dev_in, in, _len(in) * sizeof(unsigned char), cudaMemcpyKind::cudaMemcpyHostToDevice);
 
-	cudaStatus = cudaMallocManaged((void**)& dev_final_out, NUM_OF_THREADS * sizeof(unsigned char));
+	cudaStatus = cudaMallocManaged((void**)& dev_final_out, _len(in) * sizeof(uint8_t));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
 		goto Error;
 	}
+
+	//cudaMemset(dev_final_out, 0, _len(in));
 
 	cudaStatus = cudaMallocManaged((void**)& x, sizeof(size_t) * 2 + 512 * sizeof(lzw_enc_t));
 	if (cudaStatus != cudaSuccess) {
