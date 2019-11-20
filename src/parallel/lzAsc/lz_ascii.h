eb09eb007ac9d6b5a00051d2a3613f0b592ceea8
Moetassem Abdelazim <moetassem.abdelazim@mail.mcgill.ca>
2019-11-19 20:07:37 -0500

    Added parallel stitching

--- src/parallel/lzAsc/lz_ascii.h
+++ src/parallel/lzAsc/lz_ascii.h
@@ -11,9 +11,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <inttypes.h>
+#include <time.h>
 
 #define MAX_NUMBER_THREADS_PER_BLOCK 1024
-#define NUM_OF_THREADS 32
+#define NUM_OF_THREADS 512
 
 #define M_CLR 256 /* clear table marker */
 #define M_EOD 257 /* end-of-data marker */
@@ -43,9 +44,12 @@ void* pc_heap_mem_extend(void* m, size_t new_n);
 inline void _clear(void* m);
 //inline void write_bits_encoder(uint16_t x);
 
-cudaError_t lz_ascii_with_cuda(uint8_t* in, int numOfThreads);
+cudaError_t lz_ascii_with_cuda(uint8_t* in);
 
 
-__global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_block, uint8_t* dev_in, uint8_t* out_final, lzw_enc_t* dict, size_t size, int max_bits);
+__global__ void lz_encode_with_ascii_kernel(int threads_per_block, uint8_t* dev_in, int* segment_lengths, uint8_t* out, lzw_enc_t* dict, size_t size, int max_bits);
+
+__global__ void populate(int threads_per_block, int* segment_lengths, uint8_t* out, uint8_t* encoded);
+
 //__global__ void* gpu_mem_alloc(size_t item_type, size_t n_item);
 //__global__ void* gpu_mem_extend(void* m, size_t new_n);
