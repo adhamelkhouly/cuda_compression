8c06366203febf880ef643fdb8fdba6eb0ca7d28
Moetassem Abdelazim <moetassem.abdelazim@mail.mcgill.ca>
2019-11-19 20:07:37 -0500

    Added parallel stitching

--- src/parallel/lzAsc/lz_ascii.cu
+++ src/parallel/lzAsc/lz_ascii.cu
@@ -2,9 +2,12 @@
 
 /****************Cuda Functions on GPU*************************/
 //TODO: has bugs, num of threads matter for the output size ... look into the looping part and synchronization
-__global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_block, uint8_t* dev_in, uint8_t* out_final, lzw_enc_t* dict, size_t size, int max_bits)
+//TODO: maybe change length parameters to size_t
+//TODO: add sum variable to keep track of total encoded length
+__global__ void lz_encode_with_ascii_kernel(int threads_per_block, uint8_t* dev_in, int* segment_lengths, uint8_t* out, lzw_enc_t* dict, size_t size, int max_bits)
 {
-	int size_per_thread_const = (size + (num_of_threads-1))/ num_of_threads;
+	//__shared__ int seg_length_gpu[NUM_OF_THREADS];
+	int size_per_thread_const = (size + (NUM_OF_THREADS-1))/ NUM_OF_THREADS;
 	int size_per_thread_change = size_per_thread_const;
 	int segment_num = (threadIdx.x + (blockIdx.x * threads_per_block));
 
@@ -12,7 +15,7 @@ __global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_
 
 	int bits = 9, next_shift = 512;
 	uint16_t code, c, nc, next_code = M_NEW;
-	size_t out_segment_size = sizeof(size_t) * 2 + 4 * sizeof(uint16_t);
+	//size_t out_segment_size = sizeof(size_t) * 2 + 4 * sizeof(uint16_t);
 
 	if (max_bits > 15) max_bits = 15;
 	if (max_bits < 9) max_bits = 12;
@@ -28,7 +31,7 @@ __global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_
 	uint32_t tmp = 0;
 
 	//TODO: Look into making inline functions
-	for (code = *(segment_input_ptr++); --size_per_thread_change; ) {
+	for (code = *(segment_input_ptr++); size_per_thread_change > 0; --size_per_thread_change) {
 		c = *(segment_input_ptr++);
 		if (c == NULL) break;
 		if ((nc = dict[code].next[c])) //if nc is not equal to 0 after assignment then enter if statment
@@ -53,7 +56,7 @@ __global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_
 				o_bits -= 8;
 				//shifting o_bits to the right, shifting to the right means dividing by 2^(o_bits)
 				//eleminating the leftover bits on the right to write one byte to the ouput
-				out_final[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
+				out[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
 				out_len++;
 				//shift 1 to the left by o_bits, basically multiplying 1 by 2^(o_bits) ... then mask this value-1 on tmp
 				//saving the leftover bits on the right from the previous line for the next iteration
@@ -89,7 +92,7 @@ __global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_
 					o_bits -= 8;
 					//shifting o_bits to the right, shifting to the right means dividing by 2^(o_bits)
 					//eleminating the leftover bits on the right to write one byte to the ouput
-					out_final[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
+					out[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
 					out_len++;
 					//shift 1 to the left by o_bits, basically multiplying 1 by 2^(o_bits) ... then mask this value-1 on tmp
 					//saving the leftover bits on the right from the previous line for the next iteration
@@ -140,7 +143,7 @@ __global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_
 		o_bits -= 8;
 		//shifting o_bits to the right, shifting to the right means dividing by 2^(o_bits)
 		//eleminating the leftover bits on the right to write one byte to the ouput
-		out_final[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
+		out[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
 		out_len++;
 		//shift 1 to the left by o_bits, basically multiplying 1 by 2^(o_bits) ... then mask this value-1 on tmp
 		//saving the leftover bits on the right from the previous line for the next iteration
@@ -169,7 +172,7 @@ __global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_
 			o_bits -= 8;
 			//shifting o_bits to the right, shifting to the right means dividing by 2^(o_bits)
 			//eleminating the leftover bits on the right to write one byte to the ouput
-			out_final[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
+			out[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
 			out_len++;
 			//shift 1 to the left by o_bits, basically multiplying 1 by 2^(o_bits) ... then mask this value-1 on tmp
 			//saving the leftover bits on the right from the previous line for the next iteration
@@ -201,7 +204,7 @@ __global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_
 			o_bits -= 8;
 			//shifting o_bits to the right, shifting to the right means dividing by 2^(o_bits)
 			//eleminating the leftover bits on the right to write one byte to the ouput
-			out_final[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
+			out[(segment_num * size_per_thread_const) + out_len] = tmp >> o_bits;
 			out_len++;
 			//shift 1 to the left by o_bits, basically multiplying 1 by 2^(o_bits) ... then mask this value-1 on tmp
 			//saving the leftover bits on the right from the previous line for the next iteration
@@ -210,12 +213,28 @@ __global__ void lz_encode_with_ascii_kernel(int num_of_threads, int threads_per_
 		}
 	}
 
-	//size_t* to_free = (size_t*)out_final[out_len];
 
+	segment_lengths[segment_num] = out_len;
+	_Acquires_exclusive_lock_();
+	segment_lengths[NUM_OF_THREADS] += 5;
+	_Releases_exclusive_lock_();
+	//uint8_t* final_segment_out = (uint8_t*)malloc(out_len);
+	//memcpy(final_segment_out, &out[(segment_num * size_per_thread_const)], out_len);
+	//out_ptrs[segment_num] = final_segment_out;
 	
 	//TODO: Make sure synchronizations are good
 }
 
+__global__ void populate(int threads_per_block, size_t size, int* segment_lengths, uint8_t* out, uint8_t* encoded) {
+	int segment_num = (threadIdx.x + (blockIdx.x * threads_per_block));
+	int size_per_thread_const = (size + (NUM_OF_THREADS - 1)) / NUM_OF_THREADS;
+	if (segment_num == 0) {
+		memcpy(&encoded[0], &out[(segment_num * size_per_thread_const)], segment_lengths[segment_num]);
+	}
+	else {
+		memcpy(&encoded[segment_lengths[segment_num - 1]], &out[(segment_num * size_per_thread_const)], segment_lengths[segment_num]);
+	}
+}
 //__global__ void* gpu_mem_alloc(size_t item_type, size_t n_item) {
 //	size_t* x = nullptr;
 //	cudaError_t cudaStatus = cudaMalloc((void**)& x, sizeof(size_t) * 2 + n_item * sizeof(item_type));
@@ -267,7 +286,7 @@ inline void _clear(void* m)
 
 int main(int argc, char* argv[])
 {
-	int i, fd = open("test.txt", O_RDONLY);
+	int i, fd = open("alice29.txt", O_RDONLY);
 	if (fd == -1) {
 		fprintf(stderr, "Can't read file\n");
 		return 1;
@@ -283,17 +302,20 @@ int main(int argc, char* argv[])
 
 	printf("input size:   %d\n", _len(in));
 
-	lz_ascii_with_cuda(in, NUM_OF_THREADS);
+	lz_ascii_with_cuda(in);
 
 	return 0;
 }
 
-cudaError_t lz_ascii_with_cuda(uint8_t* in, int numOfThreads)
+cudaError_t lz_ascii_with_cuda(uint8_t* in)
 {
 	//TODO: Look into array of pointers again with each pointer pointing to an array which is malloced to max size of segment
 	uint8_t* dev_in = 0;
 	uint8_t* dev_final_out = 0;
+	int * segment_lengths; //the added one is for total size of encoded msg
 	size_t* x = 0;
+	uint8_t* encoded = 0;
+	clock_t start_t, end_t;
 
 	// Choose which GPU to run on, change this on a multi-GPU system.
 	cudaError_t cudaStatus = cudaSetDevice(0);
@@ -309,6 +331,16 @@ cudaError_t lz_ascii_with_cuda(uint8_t* in, int numOfThreads)
 	}
 
 	cudaMemcpy(dev_in, in, _len(in) * sizeof(unsigned char), cudaMemcpyKind::cudaMemcpyHostToDevice);
+
+	cudaStatus = cudaMallocManaged((void**)& segment_lengths, (NUM_OF_THREADS+1) * sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+	//cudaMemset(segment_lengths, 0, (NUM_OF_THREADS + 1)*sizeof(int));
+	for (int i = 0; i < NUM_OF_THREADS+1; i++) {
+		segment_lengths[i] = 0;
+	}
 
 	cudaStatus = cudaMallocManaged((void**)& dev_final_out, _len(in) * sizeof(uint8_t));
 	if (cudaStatus != cudaSuccess) {
@@ -329,15 +361,12 @@ cudaError_t lz_ascii_with_cuda(uint8_t* in, int numOfThreads)
 	
 	lzw_enc_t* dict = (lzw_enc_t*)(x+2);
 
-	//size_t heapsize = sizeof(int) * size_t(20000) * size_t(2 * 10000);
-	//cudaDeviceSetLimit(cudaLimitMallocHeapSize, heapsize);
-
 	int numBlocks = ((NUM_OF_THREADS + (MAX_NUMBER_THREADS_PER_BLOCK - 1)) / MAX_NUMBER_THREADS_PER_BLOCK);
 	int threadsPerBlock = ((NUM_OF_THREADS + (numBlocks - 1)) / numBlocks);
 	/*************************************** Parrallel Part of Execution **********************************************/
-	//gpuTimer.Start();
-	lz_encode_with_ascii_kernel << <numBlocks, threadsPerBlock >> > (numOfThreads, threadsPerBlock, dev_in, dev_final_out, dict, _len(in), 9);
-	//gpuTimer.Stop();
+	start_t = clock();
+	lz_encode_with_ascii_kernel << <numBlocks, threadsPerBlock >> > (threadsPerBlock, dev_in, segment_lengths, dev_final_out, dict, _len(in), 9);
+	end_t = clock();
 	/*****************************************************************************************************************/
 	//printf("-- Number of Threads: %d -- Execution Time (ms): %g \n", numOfThreads, gpuTimer.Elapsed());
 	// Check for any errors launching the kernel
@@ -355,35 +384,65 @@ cudaError_t lz_ascii_with_cuda(uint8_t* in, int numOfThreads)
 		goto Error;
 	}
 
-	//stitching shits together sequentially
-	int segment_length = 0;
-	uint8_t* tmp_out = (uint8_t*)malloc(_len(in));
-	memset(tmp_out, 0, _len(in));
-	int total_outsize = 0;
-	int segment_size = (_len(in) + (NUM_OF_THREADS - 1)) / NUM_OF_THREADS;
-	for (int x = 0; x < NUM_OF_THREADS; x++) {
-		int segment_outlen = 0;
-		while (dev_final_out[(x * segment_size) + segment_outlen] != NULL) {
-			tmp_out[total_outsize] = dev_final_out[(x * segment_size) + segment_outlen];
-			total_outsize++;
-			segment_outlen++;
-		}
-		/*if (dev_final_out[(x * segment_size) + segment_outlen] == NULL) {
-			total_outsize--;
-			segment_outlen--;
-			tmp_out[total_outsize] = 0;
-			printf("testing");
-		}*/
+	cudaStatus = cudaMallocManaged((void**)& encoded, segment_lengths[NUM_OF_THREADS] * sizeof(uint8_t));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
 	}
-	uint8_t* final_out = (uint8_t*)malloc(total_outsize);
-	memcpy(final_out, tmp_out, total_outsize);
-	free(tmp_out);
-	printf("%i", total_outsize);
+	
+	/*************************************** Parrallel Part of Execution **********************************************/
+	populate << <numBlocks, threadsPerBlock >> > (threadsPerBlock, _len(in), segment_lengths, dev_final_out, encoded);
+	/*****************************************************************************************************************/
+
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		goto Error;
+	}
+
+	// cudaDeviceSynchronize waits for the kernel to finish, and returns
+	// any errors encountered during the launch.
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "returned error code %d after launching !\n", cudaStatus);
+		goto Error;
+	}
+
+	//stitching shits together sequentially
+	//int segment_length = 0;
+	//uint8_t* tmp_out = (uint8_t*)malloc(_len(in));
+	//memset(tmp_out, 0, _len(in));
+	//int total_outsize = 0;
+	//int segment_size = (_len(in) + (NUM_OF_THREADS - 1)) / NUM_OF_THREADS;
+	//for (int x = 0; x < NUM_OF_THREADS; x++) {
+	//	int segment_outlen = 0;
+	//	while (dev_final_out[(x * segment_size) + segment_outlen] != NULL) {
+	//		tmp_out[total_outsize] = dev_final_out[(x * segment_size) + segment_outlen];
+	//		total_outsize++;
+	//		segment_outlen++;
+	//	}
+	//	/*if (dev_final_out[(x * segment_size) + segment_outlen] == NULL) {
+	//		total_outsize--;
+	//		segment_outlen--;
+	//		tmp_out[total_outsize] = 0;
+	//		printf("testing");
+	//	}*/
+	//}
+	//uint8_t* final_out = (uint8_t*)malloc(total_outsize);
+	//memcpy(final_out, tmp_out, total_outsize);
+	//free(tmp_out);
+
+	printf("\n time taken: %ld: \n", end_t - start_t);
+	//printf("%i", total_outsize);
 
 	//TODO: hopefully we can decode segments of equal sizes, or should we keep M_EOD
 	FILE* encodedFile = fopen("encoded_file.txt", "wb");
 	//for (int i = 0; i < _len(in); i = i - 256) {
-	fwrite(final_out, 1, total_outsize, encodedFile);
+	int sum = 0;
+	for (int z = 0; z < NUM_OF_THREADS; z++) {
+		sum += segment_lengths[z];
+	}
+	fwrite(encoded, sum, 1, encodedFile);
 	//}
 	
 Error:
