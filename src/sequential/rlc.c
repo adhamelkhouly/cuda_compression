dfcee72fd0c5c674631f5c9f90083315a0fd55c3
sbakhit <saleh.bakhit@hotmail.com>
2019-11-23 13:30:53 -0500

    add better huffman implementation

--- src/sequential/rlc.c
+++ src/sequential/rlc.c
@@ -1,38 +1,38 @@
 #include "rlc.h"
 
-char* rlc(char* input_str) 
-{     
-    int run_length; 
-    char count[MAX_RLEN]; 
-    int len = strlen(input_str); 
-  
-    char* dest = (char*)malloc(sizeof(char) * (len * 2 + 1)); 
-  
-    int i, j = 0, k; 
-  
-    // Traverse the input 
-    for (i = 0; i < len; i++) { 
-  
-        // Copy the first occurrence of the new character 
-        dest[j++] = input_str[i]; 
-  
+char* rlc(char* input_str)
+{
+    int run_length;
+    char count[MAX_RLEN];
+    int len = strlen(input_str);
+
+    char* dest = (char*)malloc(sizeof(char) * (len * 2 + 1));
+
+    int i, j = 0, k;
+
+    // Traverse the input
+    for (i = 0; i < len; i++) {
+
+        // Copy the first occurrence of the new character
+        dest[j++] = input_str[i];
+
         // Count the number of occurrences of the new character
-        run_length = 1; 
-        while (i + 1 < len && input_str[i] == input_str[i + 1]) { 
-            run_length++; 
-            i++; 
-        } 
+        run_length = 1;
+        while (i + 1 < len && input_str[i] == input_str[i + 1]) {
+            run_length++;
+            i++;
+        }
 
         // Copy Run length to a count[]
-        sprintf(count, "%d", run_length); 
-  
-        for (k = 0; *(count + k); k++, j++) { 
-            dest[j] = count[k]; 
-        } 
-    } 
-  
-    // Terminate string
-    dest[j] = '\0'; 
+        sprintf(count, "%d", run_length);
 
-    return dest; 
-} 
+        for (k = 0; *(count + k); k++, j++) {
+            dest[j] = count[k];
+        }
+    }
+
+    // Terminate string
+    dest[j] = '\0';
+
+    return dest;
+}
--- src/sequential/rlc.h
+++ src/sequential/rlc.h
@@ -1,7 +1,7 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAX_RLEN 50 
+#define MAX_RLEN 50
 
 char* rlc(char* input_file);
